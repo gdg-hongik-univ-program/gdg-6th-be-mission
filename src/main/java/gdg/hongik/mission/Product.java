5984dcb1c00d9feb87e2acaa3aa0df0c060d930e
chjpooh2002 <chjpooh2002@naver.com>
2026-05-03 19:27:58 +0900

    Product.java에 주석 추가 및 ProductStore.java 생성

--- src/main/java/gdg/hongik/mission/Product.java
+++ src/main/java/gdg/hongik/mission/Product.java
@@ -6,8 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Product {
-    private String productId;
-    private String productName;
-    private int productPrice;
-    private int remainQuantity;
+    private String productId;  // 상품Id
+    private String productName;   // 상품 이름
+    private int productPrice;    // 상품 하나당 가격
+    private int remainQuantity;   // 현재 상품의 개수
 }
--- src/main/java/gdg/hongik/mission/ProductStore.java
+++ src/main/java/gdg/hongik/mission/ProductStore.java
@@ -1,4 +1,10 @@
 package gdg.hongik.mission;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductStore {
+
+    public static List<Product> products = new ArrayList<>();  // product를 담는 리스트
+    public static long sequence = 1L;
 }
