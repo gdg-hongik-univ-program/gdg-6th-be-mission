01cc25362a314a2a4041cba43a601f743b345717
chjpooh2002 <chjpooh2002@naver.com>
2026-05-10 11:08:13 +0900

    ProductUserService 코드 완성 및 ProductUserController 코드 수정

--- src/main/java/gdg/hongik/mission/controller/ProductUserController.java
+++ src/main/java/gdg/hongik/mission/controller/ProductUserController.java
@@ -1,84 +1,22 @@
 package gdg.hongik.mission.controller;
-import gdg.hongik.mission.entity.Product;
 import org.springframework.web.bind.annotation.*;
-
+import gdg.hongik.mission.service.ProductUserService;
 import java.util.*;
 
 @RestController
 public class ProductUserController {
+    private final ProductUserService productUserService;
 
-    // 1. 상품 조회
-    @GetMapping("/products/{productId}")
-    public Product getProduct(@PathVariable Long productId) {
-
-        Product product = null;
-
-        for (Product p : ProductStore.products) {
-            if (p.getProductId().equals(productId)) {
-                product = p;
-                break;
-            }
-        }
-
-        if (product == null) {
-            throw new RuntimeException("Product not found");
-        }
-
-        return product;
+    public ProductUserController(ProductUserService productUserService) {
+        this.productUserService = productUserService;
     }
 
-    // 2. 상품 구매
+    // 상품 구매 요청
     @PostMapping("/orders")
-    public Map<String, Object> purchase(@RequestBody Map<String, Object> request) {
+    public Map<String, Object> purchase(
+            @RequestBody Map<String, Object> request
+    ) {
 
-        List<Map<String, Object>> orderProducts =
-                (List<Map<String, Object>>) request.get("orderProducts");
-
-        int totalAmount = 0;
-        List<Map<String, Object>> result = new ArrayList<>();
-
-        for (int i = 0; i < orderProducts.size(); i++) {
-
-            Map<String, Object> item = orderProducts.get(i);
-
-            Long productId = Long.valueOf(item.get("productId").toString());
-            int quantity = (int) item.get("quantity");
-
-            Product product = null;
-
-            for (Product p : ProductStore.products) {
-                if (p.getProductId().equals(productId)) {
-                    product = p;
-                    break;
-                }
-            }
-
-            if (product == null) {
-                throw new RuntimeException("Product not found");
-            }
-
-            if (product.getRemainQuantity() < quantity) {
-                throw new RuntimeException("Not enough stock");
-            }
-
-            product.setRemainQuantity(product.getRemainQuantity() - quantity);
-
-            int amount = product.getProductPrice() * quantity;
-            totalAmount += amount;
-
-            Map<String, Object> temp = new HashMap<>();
-            temp.put("productName", product.getProductName());
-            temp.put("quantity", quantity);
-            temp.put("amount", amount);
-
-            result.add(temp);
-        }
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("orderId", "Ord" + System.currentTimeMillis()); // 매번 다른 주문Id를 생성하기 위해
-        response.put("price", totalAmount);
-        response.put("orderProducts", result);
-
-        return response;
+        return productUserService.purchase(request);
     }
 }
--- src/main/java/gdg/hongik/mission/service/ProductUserService.java
+++ src/main/java/gdg/hongik/mission/service/ProductUserService.java
@@ -2,9 +2,71 @@ package gdg.hongik.mission.service;
 import gdg.hongik.mission.entity.Product;
 import gdg.hongik.mission.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.*;
 
 @Service
 public class ProductUserService {
+
+    private final ProductRepository productRepository;
+
+    public ProductUserService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Map<String, Object> purchase( Map<String, Object> request) {
+
+        List<Map<String, Object>> orderProducts =
+                (List<Map<String, Object>>) request.get("orderProducts");
+
+        int totalAmount = 0;
+        List<Map<String, Object>> result = new ArrayList<>();
+
+        List<Product> products = productRepository.findAll();
+
+        for (int i = 0; i < orderProducts.size(); i++) {
+
+            Map<String, Object> item = orderProducts.get(i);
+
+            Long productId = Long.valueOf(item.get("productId").toString());
+            int quantity = (int) item.get("quantity");
+
+            Product product = null;
+
+            for (Product p : products) {
+                if (p.getProductId().equals(productId)) {
+                    product = p;
+                    break;
+                }
+            }
+
+            if (product == null) {
+                throw new RuntimeException("Product not found");
+            }
+
+            if (product.getRemainQuantity() < quantity) {
+                throw new RuntimeException("Not enough stock");
+            }
+
+            product.setRemainQuantity(product.getRemainQuantity() - quantity);
+
+            int amount = product.getProductPrice() * quantity;
+            totalAmount += amount;
+
+            Map<String, Object> temp = new HashMap<>();
+            temp.put("productName", product.getProductName());
+            temp.put("quantity", quantity);
+            temp.put("amount", amount);
+
+            result.add(temp);
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("orderId", "Ord" + System.currentTimeMillis()); // 매번 다른 주문Id를 생성하기 위해
+        response.put("price", totalAmount);
+        response.put("orderProducts", result);
+
+        return response;
+    }
 }
